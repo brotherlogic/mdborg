cf8a256fc3913f74df7aa887515158d86d3887f3
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2010-08-28 15:36:51 +0100

    Added the processor

---
+++ src/main/java/uk/co/brotherlogic/mdb/Processor.java
@@ -0,0 +1,165 @@
+package uk.co.brotherlogic.mdb;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.blinkenlights.jid3.ID3Exception;
+import org.blinkenlights.jid3.MP3File;
+import org.blinkenlights.jid3.v2.ID3V2Tag;
+import org.blinkenlights.jid3.v2.ID3V2_3_0Tag;
+
+import uk.co.brotherlogic.mdb.record.GetRecords;
+import uk.co.brotherlogic.mdb.record.Record;
+
+/**
+ * Module for processing the files
+ * 
+ * @author simon
+ * 
+ */
+public class Processor {
+
+	private void checkFile(File f, Record r, int trackNumber)
+			throws IOException, ID3Exception, SQLException {
+
+		MP3File file = new MP3File(f);
+		file.removeID3V1Tag();
+
+		ID3V2Tag tags = file.getID3V2Tag();
+		boolean changed = false;
+		if (tags != null) {
+			System.err.println(tags);
+			if (tags.getArtist() == null
+					|| !tags.getArtist().equals(
+							r.getFormTrackArtist(trackNumber))) {
+				tags.setArtist(r.getFormTrackArtist(trackNumber));
+				changed = true;
+			}
+
+			if (tags.getTitle() == null
+					|| !tags.getTitle()
+							.equals(r.getFormTrackTitle(trackNumber))) {
+				tags.setTitle(r.getFormTrackTitle(trackNumber));
+				changed = true;
+			}
+
+			if (tags.getAlbum() == null
+					|| !tags.getAlbum().equals(r.getTitle())) {
+				tags.setAlbum(r.getTitle());
+				changed = true;
+			}
+
+			try {
+				if (tags.getTrackNumber() <= 0
+						|| tags.getTrackNumber() != trackNumber) {
+					tags.setTrackNumber(trackNumber);
+					changed = true;
+				}
+			} catch (ID3Exception e) {
+				tags.setTrackNumber(trackNumber);
+				changed = true;
+			}
+
+			if (changed) {
+				System.err.println("ADJUST: " + f);
+				file.setID3Tag(tags);
+				file.sync();
+			}
+		} else {
+			System.err.println("SETTING THE TAGS");
+			ID3V2_3_0Tag tag = new ID3V2_3_0Tag();
+			tag.setArtist(r.getFormTrackArtist(trackNumber));
+			tag.setAlbum(r.getTitle());
+			tag.setTrackNumber(trackNumber);
+			tag.setTitle(r.getFormTrackTitle(trackNumber));
+			file.setID3Tag(tag);
+			file.sync();
+		}
+
+	}
+
+	private File moveDirectory(File f, Record r) throws IOException {
+		File baseDir = f.getParentFile();
+		File outDir = new File(Organiser.BASE_LOC + r.getFileAdd());
+
+		String procString = "mv \"" + baseDir.getAbsolutePath() + "\" \""
+				+ outDir.getAbsolutePath() + "\"";
+		System.out.println("Running: " + procString);
+		Runtime.getRuntime().exec(procString);
+		return new File(outDir, "CDout.txt");
+	}
+
+	public void process(File f) throws IOException, SQLException {
+		// Read the CDOut file
+		String[] lines = readLines(f);
+		if (lines.length == 1)
+			process(f, Integer.parseInt(lines[0]));
+		else
+			process(f, Integer.parseInt(lines[3]));
+	}
+
+	private void process(File f, int number) throws IOException, SQLException {
+		Record r = GetRecords.create().getRecord(number);
+
+		String base = r.getFileAdd();
+		String aBase = f.getParentFile().getAbsolutePath().substring(
+				Organiser.BASE_LOC.length());
+
+		// Check the directory
+		if (!base.equals(aBase)) {
+			System.out.println("Directory mismatch");
+			f = moveDirectory(f, r);
+		}
+
+		// Now check the track names
+		for (File trackFile : f.getParentFile().listFiles())
+			if (trackFile.getName().endsWith(".mp3")) {
+				String strFilename = trackFile.getAbsolutePath().substring(
+						Organiser.BASE_LOC.length() + r.getFileAdd().length()
+								+ 1);
+				int tNumber = Integer.parseInt(strFilename.substring(0, 3));
+				String tRep = r.getTrackRep(tNumber) + ".mp3";
+
+				// See if we need to move the file
+				if (!strFilename.equals(tRep)) {
+					String[] mover = new String[] {
+							"mv",
+							Organiser.BASE_LOC + r.getFileAdd()
+									+ File.separator + strFilename,
+							Organiser.BASE_LOC + r.getFileAdd()
+									+ File.separator + tRep };
+					Process p = Runtime.getRuntime().exec(mover);
+				}
+
+				File checkFile = new File(Organiser.BASE_LOC + r.getFileAdd()
+						+ File.separator + tRep);
+
+				try {
+					checkFile(checkFile, r, tNumber);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
+	}
+
+	private String[] readLines(File f) {
+		List<String> stringList = new LinkedList<String>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+			for (String line = reader.readLine(); line != null; line = reader
+					.readLine())
+				stringList.add(line.trim());
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return stringList.toArray(new String[1]);
+	}
+}
