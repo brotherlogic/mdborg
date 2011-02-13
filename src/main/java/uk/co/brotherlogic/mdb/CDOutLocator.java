be3333a99858ccd56b8d760b9ab0f38c70617660
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-02-13 07:35:27 +0000

    Added a command line string qualifier

--- src/main/java/uk/co/brotherlogic/mdb/CDOutLocator.java
+++ src/main/java/uk/co/brotherlogic/mdb/CDOutLocator.java
@@ -10,35 +10,38 @@ import java.util.List;
  * @author simon
  * 
  */
-public class CDOutLocator {
+public class CDOutLocator
+{
 
-	/**
-	 * Method to search the music directory to locate CDOut files
-	 * 
-	 * @return A list of all the CDOut files
-	 */
-	public final List<File> getLocations() {
-		List<File> cdOutFiles = new LinkedList<File>();
+   /**
+    * Method to search the music directory to locate CDOut files
+    * 
+    * @return A list of all the CDOut files
+    */
+   public final List<File> getLocations(String qualifier)
+   {
+      List<File> cdOutFiles = new LinkedList<File>();
 
-		searchBase(new File(Organiser.BASE_LOC), cdOutFiles);
+      searchBase(new File(Organiser.BASE_LOC), cdOutFiles, qualifier);
 
-		return cdOutFiles;
-	}
+      return cdOutFiles;
+   }
 
-	/**
-	 * Searches a base location for CDOut files
-	 * 
-	 * @param loc
-	 *            The location to search from
-	 * @param cdOutFiles
-	 *            The list to add to
-	 */
-	private void searchBase(final File loc, final List<File> cdOutFiles) {
-		for (File f : loc.listFiles())
-			if (f.isDirectory())
-				searchBase(f, cdOutFiles);
-			else if (f.getName().equals("CDout.txt") && f.getAbsolutePath().contains("Jazz"))
-				cdOutFiles.add(f);
-	}
+   /**
+    * Searches a base location for CDOut files
+    * 
+    * @param loc
+    *           The location to search from
+    * @param cdOutFiles
+    *           The list to add to
+    */
+   private void searchBase(final File loc, final List<File> cdOutFiles, String qualifier)
+   {
+      for (File f : loc.listFiles())
+         if (f.isDirectory())
+            searchBase(f, cdOutFiles, qualifier);
+         else if (f.getName().equals("CDout.txt") && f.getAbsolutePath().contains(qualifier))
+            cdOutFiles.add(f);
+   }
 
 }
--- src/main/java/uk/co/brotherlogic/mdb/Organiser.java
+++ src/main/java/uk/co/brotherlogic/mdb/Organiser.java
@@ -2,54 +2,62 @@ package uk.co.brotherlogic.mdb;
 
 import java.io.File;
 import java.util.List;
-import uk.co.brotherlogic.mdb.Connect;
 
 /**
  * Main entry for the mdborg application
  * 
  */
-public class Organiser {
+public class Organiser
+{
 
-	/** The base location for all the music files */
-	public static final String BASE_LOC = "/usr/share/music/music/";
+   /** The base location for all the music files */
+   public static final String BASE_LOC = "/usr/share/music/music/";
 
-	/** To avoid magic numbers */
-	private static final double MS_IN_A_S = 1000.0;
+   /** To avoid magic numbers */
+   private static final double MS_IN_A_S = 1000.0;
 
-	/**
-	 * Main method
-	 * 
-	 * @param args
-	 *            no arguments
-	 */
-	public static void main(final String[] args) throws Exception{
-		Organiser org = new Organiser();
+   /**
+    * Main method
+    * 
+    * @param args
+    *           no arguments
+    */
+   public static void main(final String[] args) throws Exception
+   {
+      Organiser org = new Organiser();
 
-		long sTime = System.currentTimeMillis();
-		org.run();
-		Connect.getConnection().commitTrans();
-		System.out.println("Complete in "
-				+ (System.currentTimeMillis() - sTime) / MS_IN_A_S);
-	}
+      long sTime = System.currentTimeMillis();
+      if (args.length == 1)
+         org.run(args[0]);
+      else
+         org.run("");
+      Connect.getConnection().commitTrans();
+      System.out.println("Complete in " + (System.currentTimeMillis() - sTime) / MS_IN_A_S);
+   }
 
-	/** Out file locator */
-	private final CDOutLocator locator = new CDOutLocator();
+   /** Out file locator */
+   private final CDOutLocator locator = new CDOutLocator();
 
-	/** THe processor */
-	private final Processor proc = new Processor();
+   /** THe processor */
+   private final Processor proc = new Processor();
 
-	/**
-	 * Method to do all the work
-	 */
-	public final void run() {
-		List<File> cdOutFiles = locator.getLocations();
-		for (File f : cdOutFiles) {
-			try {
-				proc.process(f);
-			} catch (Exception e) {
-				System.err.println("Error in : " + f);
-				e.printStackTrace();
-			}
-		}
-	}
+   /**
+    * Method to do all the work
+    */
+   public final void run(String qualifier)
+   {
+      List<File> cdOutFiles = locator.getLocations(qualifier);
+      for (File f : cdOutFiles)
+      {
+         try
+         {
+            proc.process(f);
+         }
+         catch (Exception e)
+         {
+            System.err.println("Error in : " + f);
+            e.printStackTrace();
+         }
+      }
+   }
 }
